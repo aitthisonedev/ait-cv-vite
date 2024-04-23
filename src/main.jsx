50a00d03a02314926595f1796329168e76411d88
aitthisonedev <aitthisonedev@gmail.com>
2024-04-23 19:57:50 +0700

    updaate

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App.jsx'
 import Navbar from './Nabar/Navbar.jsx';
-import Footer from './Footer/Footer.jsx';
+// import Footer from './Footer/Footer.jsx';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Route path='/' element={<App/>} />
     </Routes>
     <div className="Last-text">
-<Footer/>
+{/* <Footer/> */}
         <p>Developed with love by Aittisone Duangsompheang <i className="fa-solid fa-laptop-code"style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }} ></i> @ 2024</p>
     </div>
   </Router>
