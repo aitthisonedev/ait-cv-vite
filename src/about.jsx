522dc352a5e5a99df33a36804848e5261253ecdb
aitthisonedev <aitthisonedev@gmail.com>
2024-05-16 14:17:48 +0700

    Update

--- src/about.jsx
+++ src/about.jsx
@@ -1,7 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import "./about.css";
-const about = () => {
+import { useTranslation } from "react-i18next";
+
+const About = () => {
+  const { t } = useTranslation();
+
   return (
     <section className="about" id="about">
       <div className="about-img">
@@ -9,28 +13,14 @@ const about = () => {
       </div>
       <div className="about-text">
         <h2>
-          About <span>Me</span>
+          <span>{t("About.me")}</span>
         </h2>
-        <h4>Full Stack Developer</h4>
+        <h4>{t("About.title")}</h4>
         <p>
-          Pioneering Web Elegance: Elevate your online presence with the finesse
-          of web development craftsmanship. It’s not just coding; it’s a
-          symphony of digital artistry!
-          <br></br>
-          <br></br>
-          Advancements in Real-Time Web Solutions: Your website should evolve as
-          swiftly as the digital landscape. Stay proactive with real-time
-          innovations, ensuring your online platform remains at the forefront of
-          user experience!
-          <br></br>
-          <br></br>
-          Mastering the Web with Creative Precision: Harness the power of
-          creativity and precision in every line of code. Crafting exceptional
-          web experiences isn’t just a task; it’s a testament to your dedication
-          to excellence!
+          {t("About.Description")}
         </p>
         <a href="/#contact" className="btn-box">
-          Contact
+        {t("About.Contact")}
         </a>
         <div className="about-Education">{/* <Education /> */}</div>
       </div>
@@ -38,4 +28,4 @@ const about = () => {
   );
 };
 
-export default about;
+export default About;
